5e1a9003e0e85c3824446b69ac60ab34bf50f6fa
Derek Maffett <derekgmaffett@gmail.com>
2014-12-10 23:41:44 -0800

    refactor solution for pascals triangle

--- derekmaffett/lib/exercises/pascals_triangle.rb
+++ derekmaffett/lib/exercises/pascals_triangle.rb
@@ -1,4 +1,5 @@
 module LeetCode
+  # Implementation of Pascal's Triangle using recursion and caching
   class PascalsTriangle
     def initialize
       @triangle = []
@@ -6,24 +7,28 @@ module LeetCode
 
     def generate(num)
       triangle = Array.new(num) { [] }
-      triangle.map.with_index { |level, index| calculate_level(index + 1) }
+      triangle.map.with_index { |_level, index| calculate_level(index + 1) }
     end
 
     def calculate_level(level)
       return [1] if level == 1
+
       cache = @triangle[level - 1]
       return cache if cache
 
       current = Array.new(level)
       previous = calculate_level(level - 1)
-      current[0] = previous.first
-      current[level - 1] = previous.last
 
-      1.upto(level - 2) { |i| current[i] = previous[i - 1] + previous[i] }
+      @triangle[level - 1] = fill_in_level(current, previous)
+    end
 
-      @triangle[level - 1] = current
+    private
+
+    def fill_in_level(current, prev)
+      current[0] = prev[0]
+      current[-1] = prev[-1]
+      1.upto(current.size - 2) { |i| current[i] = prev[i - 1] + prev[i] }
+      current
     end
   end
 end
-
-
