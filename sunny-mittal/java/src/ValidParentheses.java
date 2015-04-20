a959c47173756c523328c5a394fd041a84850452
Sunny Mittal <sunnymittal2003@gmail.com>
2015-04-20 16:31:32 -0700

    Valid Parentheses

---
+++ sunny-mittal/java/src/ValidParentheses.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class ValidParentheses {
+    public static void main(String[] args) {
+        System.out.println(isValid("()"));
+    }
+
+    public static boolean isValid(String s) {
+        Map<Character,Character> pairs = new HashMap<Character,Character>();
+        pairs.put('(', ')'); pairs.put('{', '}'); pairs.put('[', ']');
+
+        Stack<Character> seen = new Stack<Character>();
+        for (char c : s.toCharArray()) {
+            if (pairs.containsKey(c))
+                seen.push(c);
+            else if (pairs.containsValue(c))
+                if(seen.empty() || pairs.get(seen.pop()) != c)
+                    return false;
+        }
+        return seen.isEmpty() ? true : false;
+    }
+}
